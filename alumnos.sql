3f6f3e59514f6cb247cfe3284cc63be5e7775417
carlosarizmend-lab <carlos.arizmendi@uinenlinea.mx>
2025-10-25 15:27:45 +0000

    Tablas creadas

---
+++ alumnos.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS alumnos (matricula VARCHAR(10) PRIMARY KEY,
+                                    nombre VARCHAR(100) NOT NULL,
+                                    apellido_p VARCHAR(100) NOT NULL, 
+                                    edad INT NOT NULL, 
+                                    genero CHAR NOT NULL, trabajo BOOLEAN NOT NULL);
+CREATE TABLE IF NOT EXISTS escuela(
+    id_plantel VARCHAR(50) PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(50) NOT NULL);
